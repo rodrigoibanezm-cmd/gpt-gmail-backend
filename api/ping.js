c9f299b2f9baca5978bd5cd9da9878428c0e3f26
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2025-11-30 18:28:08 -0300

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
-  return res.status(200).json({ status: "ok" });
+  return res.status(200).json({
+    status: "success",
+    message: "ok",
+    data: null
+  });
 }
